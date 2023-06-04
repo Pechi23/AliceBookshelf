561358fffe7a406c50f96d3d277550b7dde43c53
George Pecherle <george.pecherle@gmail.com>
2023-06-04 16:13:54 +0300

    output text to file

--- Program.cs
+++ Program.cs
@@ -1,36 +1,44 @@
-﻿//read input
-string inputFile = "InputFile.txt";
-StreamReader sr = new StreamReader(inputFile);
+﻿string inputFilePath = "InputFile.txt";
+string outputFilePath = @"..\..\..\OutputFile.txt";
 
+//read input
 List<string> input = new List<string>();
-string inputLine;
-while ((inputLine = sr.ReadLine()) != null)
-    input.Add(inputLine);
-
-foreach(string line in input)
+using (StreamReader sr = new StreamReader(inputFilePath))
 {
-    Stack<char> stack = new Stack<char>();
-    foreach (char c in line)
-    {
-        if (c == '\\')
-        {
-            string reversed = "";
-            while (stack.Any() && stack.Peek() != '/')
-                reversed += stack.Pop();
-            if (stack.Any() && stack.Peek() == '/')
-                stack.Pop();
-            for(int i=0;i<reversed.Length;i++)
-                stack.Push(reversed[i]);
-        }
-        else
-            stack.Push(c);
-    }
-    
-    char[] reversedStack = new char[stack.Count];
-    for (int i = reversedStack.Length - 1; i >= 0; i--)
-        reversedStack[i] = stack.Pop();
+    string inputLine;
+    while ((inputLine = sr.ReadLine()) != null)
+        input.Add(inputLine);
+}
 
-   foreach(var el in reversedStack)
-        Console.Write(el);
-    Console.WriteLine();
+//solver
+using (StreamWriter sw = new StreamWriter(outputFilePath))
+{
+    
+    foreach (string line in input)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in line)
+        {
+            if (c == '\\')
+            {
+                string reversed = "";
+                while (stack.Any() && stack.Peek() != '/')
+                    reversed += stack.Pop();
+                if (stack.Any() && stack.Peek() == '/')
+                    stack.Pop();
+                for(int i=0;i<reversed.Length;i++)
+                    stack.Push(reversed[i]);
+            }
+            else
+                stack.Push(c);
+        }
+    
+        char[] reversedStack = new char[stack.Count];
+        for (int i = reversedStack.Length - 1; i >= 0; i--)
+            reversedStack[i] = stack.Pop();
+    
+        foreach (var el in reversedStack)
+            sw.Write(el);
+        sw.WriteLine();
+    }
 }
